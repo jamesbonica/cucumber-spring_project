29e4d9c0a43c30f0e66d8f3e93d924cca86fa290
James <jamesbonica@gmail.com>
2018-11-03 18:16:29 -0400

    changes made while developing Jenkins workflow

--- src/test/java/runner/F_1001_UserAuthentication_Test.java
+++ src/test/java/runner/F_1001_UserAuthentication_Test.java
@@ -6,12 +6,12 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		tags = {"@Initialize, @Scenario=test"},
+		tags = {"@Initialize, @Scenario=1"},
 		plugin = { "pretty", "html:target/html/1001_User_Authentication/", 
 				"json:target/json/1001_User_Authentication.json" }, 
 		features = {"src/test/resources/cucumber/1001_User_Authentication.feature" }, 
 		glue = {"steps" }, 
 		monochrome = true)
-public class F_1001_UserAuthentication_Runner {
+public class F_1001_UserAuthentication_Test {
 
 }
